0cdac0a4b0c345a0f281ec006dde382ec5e0343f
jonathan.sienkiewicz <jonathan.sienkiewicz@gmail.com>
2022-03-14 01:46:05 -0300

    adicionando a ultima aula - aprofundando em funcoes

---
+++ sintaxe-basica-javascript/aula6-aprofundando-em-funcoes/calculadora.js
@@ -0,0 +1,6 @@
+/* function calculadora(){
+    const OPERACAO = prompt('escolha uma operação:')
+    console.log(OPERACAO)
+}
+
+calculadora(); */
---
+++ sintaxe-basica-javascript/aula6-aprofundando-em-funcoes/tiposDeFuncao.js
@@ -0,0 +1,25 @@
+// tipos de funcao
+
+// declarativas
+
+/* function funcao(){
+    console.log('Só uma mensagem de uma funcao declarativa')
+}
+
+funcao(); */
+
+// expressão de função 
+// sem nomeação - dessa forma o JS atrinui o nome da variável à funcao
+/* var funcao = function(){
+    console.log('Só uma mensagem de funcao de expressão')
+}
+
+funcao(); */
+
+// arrow function
+
+/* var funcao = () => {
+    console.log('Sou uma arrow function')
+}
+
+funcao(); */
