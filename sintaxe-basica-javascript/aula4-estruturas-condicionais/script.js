67892fb5c2149478e79b03b972bd38e44a2a41f5
jonathan.sienkiewicz <jonathan.sienkiewicz@gmail.com>
2022-03-14 01:12:01 -0300

    adicionando aulas 4 e 5

---
+++ sintaxe-basica-javascript/aula4-estruturas-condicionais/script.js
@@ -0,0 +1,131 @@
+/* var jogador1 = 0;
+var jogador2 = 1; 
+var placar; */
+// IF, ELSE, ELSE IF, NINHO DE IF
+/* if(jogador1 != -1){
+    if (jogador1>0) {
+        console.log('Jogador 1 marcou ponto!')
+    } else if(jogador2>0) {
+        console.log('jogador 2 marcou ponto.')
+    } else {
+        console.log('ninguem marcou ponto.')
+    }
+} else {
+    console.log('placar inválido')
+} */
+
+// IF TERNÁRIO
+
+/* jogador1 != -1 && jogador2 != -1 ? console.log('jogadores são válidos'):
+console.log('jogadores inválidos ')
+
+    if (jogador1>0 && jogador2 == 0) {
+        console.log('Jogador 1 marcou ponto!')
+        placar = jogador1>jogador2;
+    } else if(jogador2>0 && jogador1 ==0) {
+        console.log('jogador 2 marcou ponto.')
+        placar = jogador2>jogador1;
+    } else {
+        console.log('ninguem marcou ponto.')
+    } */
+
+// SWITCH CASE
+
+/* switch(placar){
+    case placar = jogador1>jogador2:
+        console.log('jogador 1 ganhou');
+    break;
+    case placar = jogador2>jogador1:
+        console.log('jogador 2 ganhou');
+    break;
+    default:
+        console.log('ninguem ganhou');
+} */
+
+// ESTRUTURAS DE REPETIÇÃO
+// FOR
+
+/* var array = ['valor1','valor2','valor3','valor4','valor5']
+
+var object = {propriedade1: 'valor1',propriedade2:'valor2',propriedade3:'valor3',}
+ */
+// for executa uma instrução até que ela seja falsa
+
+/* for (let indice = 0; indice < array.length; indice++) {
+    console.log(indice);
+} */
+
+// FOR IN
+
+// com array
+/* for(indice in array) {
+    console.log(indice);
+} */
+//com object
+/* for(i in object){
+    console.log(i)
+} */
+
+// FOR OF
+// com array
+/* for (i of array) {
+    console.log(i);
+} */
+// com object
+/* for(i of object.propriedade1){
+    console.log(i)
+} */
+
+// WHILE
+
+/* var a = 0;
+
+while(a < 10) {
+    a++;
+    console.log(a)
+} 
+
+do{
+    a++;
+    console.log(a)
+} while(a<10) */
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ sintaxe-basica-javascript/aula5-funcoes/script.js
@@ -0,0 +1,11 @@
+function funcao(){
+    console.log('Essa mensagem veio de uma função!')
+}
+
+funcao();
+
+function funcao2(parametro){
+    console.log(parametro)
+}
+
+funcao2('Olá, mundo!')
