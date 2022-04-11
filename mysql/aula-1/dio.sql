12cf9c0c9649337494e84a07f00aad0a03df844c
jonathan.sienkiewicz <jonathan.sienkiewicz@gmail.com>
2022-04-11 00:58:45 -0300

    inserindo aula 1 de mysql com phpmyadmin

---
+++ mysql/aula-1/dio.sql
@@ -0,0 +1,10 @@
+CREATE TABLE pessoas (
+    id INT NOT NULL PRIMARY KEY AUTOINCREMENT,
+    nome VARCHAR(30) NOT NULL,
+    nascimento DATE
+)
+
+INSERT INTO pessoas (nome, nascimento) VALUES ('Jonathan', '1991-03-05')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Nathaly', '1990-05-22')
+INSERT INTO pessoas (nome, nascimento) VALUES ('Pedro', '1995-07-17')
+
